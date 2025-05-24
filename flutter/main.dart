d00c0e798df0c6e73668384ae9342eab17c8c6cc
Summo1 <tomsummo@gmail.com>
2025-05-24 17:18:09 +1000

    js

--- flutter/main.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-  print("Hello, World!");
-  print("This is a simple Dart program.");
-  print("Let's learn Dart together!");
-  print("Dart is a client-optimized language for fast apps on any platform.");
-  print("Happy coding!");
-}
