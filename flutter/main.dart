870a46a4820cbfe4cd6e3427ffe67639ac594ee4
Summo1 <tomsummo@gmail.com>
2025-05-24 17:15:57 +1000

    do

--- flutter/main.dart
+++ flutter/main.dart
@@ -1,3 +1,7 @@
 void main(){
   print("Hello, World!");
+  print("This is a simple Dart program.");
+  print("Let's learn Dart together!");
+  print("Dart is a client-optimized language for fast apps on any platform.");
+  print("Happy coding!");
 }
