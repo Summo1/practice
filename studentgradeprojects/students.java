397b4ae81c1caef0592e80382e89c04e66017d18
Summo1 <tomsummo@gmail.com>
2025-08-08 18:31:02 +1000

    Create students.java

---
+++ studentgradeprojects/students.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class students {
+	public static void main(String[] args) {
+        
+    }
+
+    private String name;
+    private String ID;
+    private ArrayList
+
+    public students(String name, String ID){
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
