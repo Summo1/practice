2911a74aaab07c0fd6251261efa14c9fc7616de1
Summo1 <tomsummo@gmail.com>
2025-08-09 17:00:18 +1000

    Update students.java

--- studentgradeprojects/students.java
+++ studentgradeprojects/students.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.io.*;
 
 public class students {
 	public static void main(String[] args) {
@@ -8,14 +7,41 @@ public class students {
 
     private String name;
     private String ID;
-    private ArrayList
+    private ArrayList<ArrayList<String>> grades;
 
     public students(String name, String ID){
         this.name = name;
         this.ID = ID;
+        this.grades = new ArrayList<>();
     }
 
     public String getName(){
         return name;
     }
+
+    public String getId(){
+        return ID;
+    }
+
+    public ArrayList<ArrayList<String>> getGrades(){
+        return grades;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public void setId(String newID){
+        this.ID = newID;
+    }
+
+    public void setGrades(String subject, String grade){
+        for (int i = 0; i < grades.size(); i++){
+            if (grades.get(i).get(0) == subject){
+                grades.get(i).set(1, grade);
+            }
+        }
+        
+    }
+
 }
