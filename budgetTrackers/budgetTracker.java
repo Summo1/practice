6a6df086df810786c35bebd14eb5f36cf3261d54
Summo1 <tomsummo@gmail.com>
2025-08-10 11:43:41 +1000

    Create budgetTracker.java

---
+++ budgetTrackers/budgetTracker.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class budgetTracker {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click Here");
+        button.setBounds(100, 100, 50, 50);
+    }
+
+
+}
